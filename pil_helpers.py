7a22f5601467b7d2be3e555eb82bdffdf908ed77
Ryan Vilbrandt <ryan.vilbrandt@gmail.com>
2023-03-14 18:43:28 -0700

    Add more monk abilities

--- pil_helpers.py
+++ pil_helpers.py
@@ -16,50 +16,20 @@ def open_image(filepath: str) -> Image:
     return Image.open(filepath)
 
 
-def build_font(font_name, font_size):
+def build_font(font_name, font_size) -> ImageFont:
     return ImageFont.truetype(font_name, font_size)
 
 
 class TextBox:
 
     def __init__(self, x, y, w, h, halign: HAlign = HAlign.CENTER, valign: VAlign = VAlign.CENTER,
-                 font_name=DEFAULT_FONT, font_size=50, rotate=0, use_height_for_text_wrap=False):
+                 font_name: str = DEFAULT_FONT, font_size: int = 50, rotate: int = 0,
+                 use_height_for_text_wrap: bool = False, shrink_font_size_to_fit: bool = False):
         self.x, self.y, self.width, self.height = x, y, w, h
         self.halign, self.valign, self.rotate = halign, valign, rotate
         self.use_height_for_text_wrap = use_height_for_text_wrap
-        self.font = build_font(font_name, font_size)
-
-    def draw_box(self, image, color="red", x=None, y=None, width=None, height=None):
-        """
-        Useful for figuring out where in the image a text box will land
-        """
-        if x is None:
-            x = self.x
-        if y is None:
-            y = self.y
-        if width is None:
-            width = self.width
-        if height is None:
-            height = self.height
-        draw = ImageDraw.Draw(image)
-        draw.rectangle((x, y, x + width, y + height), outline=color)
-        # Draw top-left cross
-        self.draw_cross(draw, x, y)
-        # Draw center cross
-        center_x = x + (width // 2)
-        center_y = y + (height // 2)
-        self.draw_cross(draw, center_x, center_y)
-        # Draw bottom-right cross
-        self.draw_cross(draw, x + width, y + height)
-
-    @staticmethod
-    def draw_cross(draw: ImageDraw, x: int, y: int, color="green", size=5):
-        start = (x, y - size)
-        end = (x, y + size)
-        draw.line((start, end), color)
-        start = (x - size, y)
-        end = (x + size, y)
-        draw.line((start, end), color)
+        self.shrink_font_to_fit = shrink_font_size_to_fit
+        self.font_name, self.font_size = font_name, font_size
 
     @staticmethod
     def wrap_text(text, font, max_width=0):
@@ -100,21 +70,33 @@ class TextBox:
                 temp += w + " "
         return wrapped_text + temp.strip(' ')
 
-    def get_text_block_size(self, text, leading_offset=0):
-        wrapped_text = self.wrap_text(text, self.font, self.height if self.use_height_for_text_wrap else self.width)
+    def get_text_block_size(self, text: str, font: ImageFont, leading_offset: int = 0) -> Tuple[List[str], int, int]:
+        wrapped_text = self.wrap_text(text, font, self.height if self.use_height_for_text_wrap else self.width)
         lines = wrapped_text.split('\n')
 
         # Set leading
-        leading = self.font.font.ascent + self.font.font.descent + leading_offset
+        leading = font.font.ascent + font.font.descent + leading_offset
 
         # Get max line width
         max_line_width = 0
         for line in lines:
-            line_width, line_height = self.font.getsize(line)
+            line_width, line_height = font.getsize(line)
             # Keep track of the longest line width
             max_line_width = max(max_line_width, line_width)
 
-        return max_line_width, len(lines) * leading
+        return lines, max_line_width, len(lines) * leading
+
+    def shrink_font_until_text_fits(self, text: str, font_name: str, starting_font_size: int, width: int, height: int
+                                    ) -> Tuple[List[str], ImageFont]:
+        font_size = starting_font_size
+        while font_size > 0:
+            font = build_font(font_name, font_size)
+            text_lines, block_width, block_height = self.get_text_block_size(text, font)
+            if block_width <= width and block_height <= height:
+                return text_lines, font
+            font_size -= 1
+        else:
+            raise ValueError("Text is too big to fit in the text box at any font size")
 
     def add_text(self, image: Image, text: str, color: Union[str, Tuple[int, int, int]] = "black",
                  leading_offset: int = 0):
@@ -125,8 +107,14 @@ class TextBox:
 
         @return (int, int): Total width and height of the text block added, in pixels.
         """
-        wrapped_text = self.wrap_text(text, self.font, self.height if self.use_height_for_text_wrap else self.width)
-        lines = wrapped_text.split('\n')
+        if self.shrink_font_to_fit:
+            text_lines, font = self.shrink_font_until_text_fits(
+                text, self.font_name, self.font_size, self.width, self.height
+            )
+        else:
+            font = build_font(self.font_name, self.font_size)
+            wrapped_text = self.wrap_text(text, font, self.height if self.use_height_for_text_wrap else self.width)
+            text_lines = wrapped_text.split('\n')
 
         # Initialize layer and draw object
         layer = Image.new('L', (5000, 5000))
@@ -142,15 +130,15 @@ class TextBox:
             raise ValueError(f"Invalid halign value: {self.halign}")
 
         # Set leading
-        leading = self.font.font.ascent + self.font.font.descent + leading_offset
+        leading = font.font.ascent + font.font.descent + leading_offset
 
         # Begin laying down the lines, top to bottom
         y = start_y
         max_line_width = 0
-        for line in lines:
+        for line in text_lines:
             # If current line is blank, just change y and skip to next
             if not line == "":
-                line_width = self.font.getlength(line)
+                line_width = font.getlength(line)
                 if self.halign == HAlign.LEFT:
                     x_pos = start_x
                 elif self.halign == HAlign.CENTER:
@@ -161,10 +149,10 @@ class TextBox:
                     raise ValueError(f"Invalid halign value: {self.halign}")
                 # Keep track of the longest line width
                 max_line_width = max(max_line_width, line_width)
-                draw.text((x_pos, y), line, font=self.font, fill=255)
+                draw.text((x_pos, y), line, font=font, fill=255)
             y += leading
 
-        total_text_size = (max_line_width, len(lines) * leading)
+        total_text_size = (max_line_width, len(text_lines) * leading)
 
         # Now that the text is added to the image, find the crop points
         top = start_y
@@ -185,7 +173,7 @@ class TextBox:
         if self.rotate != 0:
             layer = layer.rotate(self.rotate, expand=True)
 
-        anchor_x, anchor_y = self.get_anchors(self.halign, self.valign)
+        anchor_x, anchor_y = get_anchors(self.x, self.y, self.width, self.height, self.halign, self.valign)
 
         # Determine the anchor point for the new layer
         layer_width, layer_height = layer.size
@@ -214,28 +202,54 @@ class TextBox:
 
         # Add debug box if the flag is set
         if DEBUG_TEXT_BOX_BORDERS:
-            self.draw_box(image)
+            draw_box(image, self.x, self.y, self.width, self.height)
 
         return total_text_size
 
-    def get_anchors(self, halign: HAlign, valign: VAlign) -> Tuple[int, int]:
-        if halign == HAlign.LEFT:
-            anchor_x = self.x
-        elif halign == HAlign.CENTER:
-            anchor_x = self.x + self.width // 2
-        elif halign == HAlign.RIGHT:
-            anchor_x = self.x + self.width
-        else:
-            raise ValueError(f"Invalid halign value: {halign}")
-        if valign == VAlign.TOP:
-            anchor_y = self.y
-        elif valign == VAlign.CENTER:
-            anchor_y = self.y + self.height // 2
-        elif valign == VAlign.BOTTOM:
-            anchor_y = self.y + self.height
-        else:
-            raise ValueError(f"Invalid valign value: {valign}")
-        return anchor_x, anchor_y
+
+def draw_box(image, x: int, y: int, width: int, height: int, color="red"):
+    """
+    Useful for figuring out where in the image a text box will land
+    """
+    draw = ImageDraw.Draw(image)
+    draw.rectangle((x, y, x + width, y + height), outline=color)
+    # Draw top-left cross
+    draw_cross(draw, x, y)
+    # Draw center cross
+    center_x = x + (width // 2)
+    center_y = y + (height // 2)
+    draw_cross(draw, center_x, center_y)
+    # Draw bottom-right cross
+    draw_cross(draw, x + width, y + height)
+
+
+def draw_cross(draw: ImageDraw, x: int, y: int, color="green", size=5):
+    start = (x, y - size)
+    end = (x, y + size)
+    draw.line((start, end), color)
+    start = (x - size, y)
+    end = (x + size, y)
+    draw.line((start, end), color)
+
+
+def get_anchors(x: int, y: int, width: int, height: int, halign: HAlign, valign: VAlign) -> Tuple[int, int]:
+    if halign == HAlign.LEFT:
+        anchor_x = x
+    elif halign == HAlign.CENTER:
+        anchor_x = x + width // 2
+    elif halign == HAlign.RIGHT:
+        anchor_x = x + width
+    else:
+        raise ValueError(f"Invalid halign value: {halign}")
+    if valign == VAlign.TOP:
+        anchor_y = y
+    elif valign == VAlign.CENTER:
+        anchor_y = y + height // 2
+    elif valign == VAlign.BOTTOM:
+        anchor_y = y + height
+    else:
+        raise ValueError(f"Invalid valign value: {valign}")
+    return anchor_x, anchor_y
 
 
 def add_class_icon(im: Image, dirname: str):
@@ -300,7 +314,8 @@ action_box = TextBox(0, 50, 67, 500, halign=HAlign.RIGHT, valign=VAlign.TOP, rot
                      font_name=os.path.join(FONTS_FOLDER, "Astoria_Sans_Extended_Bold.otf"),
                      use_height_for_text_wrap=True)
 name_box = TextBox(92, 46, 631, 82)
-description_box = TextBox(105, 150, 610, 700, font_size=32, halign=HAlign.LEFT, valign=VAlign.TOP, font_name=TEXT_FONT)
+description_box = TextBox(105, 150, 610, 700, font_size=32, halign=HAlign.LEFT, valign=VAlign.TOP, font_name=TEXT_FONT,
+                          shrink_font_size_to_fit=True)
 footnote_box = TextBox(105, 860, 610, 50, font_size=32, font_name=TEXT_FONT)
-source_box = TextBox(125, 933, 382, 82, font_name=TEXT_FONT)
+source_box = TextBox(125, 933, 382, 82, font_name=TEXT_FONT, shrink_font_size_to_fit=True)
 level_box = TextBox(560, 933, 163, 82)
